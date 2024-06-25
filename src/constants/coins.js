b2e7d3bed8e4017399dba51a6b3705c27efc4d9d
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-06-25 09:26:47 +0100

    Update coins.js

--- src/constants/coins.js
+++ src/constants/coins.js
@@ -1,5 +1,5 @@
 import * as chains from './chains';
-import popcatLogo from '../CoinLogos/popcat.png';
+import '../CoinLogos/popcat.png';
 
 const MAINNETCoins = [
  {
@@ -12,7 +12,7 @@ const MAINNETCoins = [
     name: "PopCatRock",
     abbr: "POPCAT",
     address: "0xdce5726e3bc8e1f574416978279bb0ae62ab3c15",
-    logoUrl: popcatLogo, // Replace with actual logo URL
+    logoUrl: "popcat.png", // Replace with actual logo URL
   },
   {
     name: "PopTest5",
