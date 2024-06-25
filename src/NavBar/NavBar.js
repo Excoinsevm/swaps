34b925c24e4f796c6b05171509f3af538efd4f9e
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-06-25 05:59:32 +0100

    Update NavBar.js

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
 import "./NavBar.css";
-import logo from "./path-to-your-logo.png"; // Make sure to update the path to your actual logo file
 
 const NavBar = () => {
   const [clicked, setClicked] = useState(false);
@@ -14,7 +13,7 @@ const NavBar = () => {
   return (
     <nav>
       <div className="Title">
-        <img src={logo} alt="Logo" className="navbar-logo-image" />
+        <img src="https://popcatrock.xyz/assets/popcatr.png" alt="Logo" className="navbar-logo-image" />
         <h1 className="navbar-logo">
           Alternative Uniswap Interface
         </h1>
