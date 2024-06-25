f606020616e71cf01bae82c24bd6054ada1b67ed
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-06-25 08:45:59 +0100

    Update CoinButton.js

--- src/CoinSwapper/CoinButton.js
+++ src/CoinSwapper/CoinButton.js
@@ -15,25 +15,38 @@ const useStyles = makeStyles((theme) => ({
   coinName: {
     opacity: 0.6,
   },
+  coinLogo: {
+    width: 24, // Adjust size as needed
+    height: 24, // Adjust size as needed
+    marginRight: theme.spacing(1), // Add spacing between logo and text
+    borderRadius: "50%", // Ensure the logo is circular
+  },
 }));
 
 CoinButton.propTypes = {
   coinName: PropTypes.string.isRequired,
   coinAbbr: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  logoUrl: PropTypes.string.isRequired, // Add logoUrl to PropTypes
 };
 
 export default function CoinButton(props) {
-  const { coinName, coinAbbr, onClick, ...other } = props;
+  const { coinName, coinAbbr, onClick, logoUrl } = props;
   const classes = useStyles();
 
   return (
     <ButtonBase focusRipple className={classes.button} onClick={onClick}>
-      <Grid container direction="column">
-        <Typography variant="h6">{coinAbbr}</Typography>
-        <Typography variant="body2" className={classes.coinName}>
-          {coinName}
-        </Typography>
+      <Grid container direction="row" alignItems="center">
+        {/* Token Logo */}
+        <img src={logoUrl} alt={`${coinAbbr} Logo`} className={classes.coinLogo} />
+
+        {/* Token Abbreviation and Name */}
+        <Grid item>
+          <Typography variant="h6">{coinAbbr}</Typography>
+          <Typography variant="body2" className={classes.coinName}>
+            {coinName}
+          </Typography>
+        </Grid>
       </Grid>
     </ButtonBase>
   );
