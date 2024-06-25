b7f877be4bfdd834ca109d312a7e6c1213aea4ad
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-06-25 16:21:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from "react";
-import "./App.css";
-import { ethers } from "ethers";
-import Web3Provider from "./network";
-import NavBar from "./NavBar/NavBar";
-import CoinSwapper from "./CoinSwapper/CoinSwapper";
-import { Route } from "react-router-dom";
-import { SnackbarProvider } from "notistack";
-import Liquidity from "./Liquidity/Liquidity";
-import { createTheme, ThemeProvider } from "@material-ui/core";
+// App.js
+
+import React from 'react';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import { SnackbarProvider } from 'notistack';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import Web3Provider from './network';
+import NavBar from './NavBar/NavBar';
+import CoinSwapper from './CoinSwapper/CoinSwapper';
+import Liquidity from './Liquidity/Liquidity';
+import HomePage from './HomePage'; // Import the HomePage component
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#ff0000",
-      contrastText: "#ffffff",
+      main: '#ff0000',
+      contrastText: '#ffffff',
     },
     secondary: {
-      main: "#9e9e9e",
-      contrastText: "#ffffff",
+      main: '#9e9e9e',
+      contrastText: '#ffffff',
     },
   },
 });
@@ -31,16 +33,20 @@ const App = () => {
             render={(network) => (
               <div>
                 <NavBar />
-                <Route exact path="/swap/">
-                  <CoinSwapper network={network} />
-                </Route>
-
-                <Route exact path="/liquidity/">
-                  <Liquidity network={network} />
-                </Route>
+                <Switch>
+                  <Route exact path="/">
+                    <HomePage />
+                  </Route>
+                  <Route exact path="/swap/">
+                    <CoinSwapper network={network} />
+                  </Route>
+                  <Route exact path="/liquidity/">
+                    <Liquidity network={network} />
+                  </Route>
+                </Switch>
               </div>
             )}
-          ></Web3Provider>
+          />
         </ThemeProvider>
       </SnackbarProvider>
     </div>
