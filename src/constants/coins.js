cda133c6341a5f89bcac38a9e6ba92e16c8f8c2b
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-06-25 15:31:52 +0100

    Update coins.js

--- src/constants/coins.js
+++ src/constants/coins.js
@@ -13,12 +13,6 @@ const MAINNETCoins = [
     address: "0xdce5726e3bc8e1f574416978279bb0ae62ab3c15",
     logoUrl: "https://i.imgur.com/qWwaaY9.jpeg", // Replace with actual logo URL
   },
-  {
-    name: "PopTest5",
-    abbr: "POP5",
-    address: "0xa54DCe9037f64cF2Bf60EF4602cdF65d3095D844",
-    logoUrl: "https://example.com/poptest5-logo.png", // Replace with actual logo URL
-  },
 ]
 
 const COINS = new Map();
