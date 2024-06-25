6c6210e285147d7b26e37c2b4f226d6f3df1cf08
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-06-26 00:56:03 +0100

    Update chains.js

--- src/constants/chains.js
+++ src/constants/chains.js
@@ -1,9 +1,17 @@
-export const networks = [7171]
-
-export const ChainId = {
-  MAINNET: 7171,
+export const network = {
+  id: 7171,
+  rpcUrl: "https://connect.bit-rock.io",
+  name: "Bitrock",
+  explorerUrl: "https://explorer.bit-rock.io",
+  nativeCurrency: {
+    name: "Bitrock",
+    symbol: "BROCK",
+    decimals: 18,
+  },
 };
 
-export const routerAddress = new Map();
-routerAddress.set(ChainId.MAINNET, "0x6789f48F8ED4506362592Ca58E9c160d624932B6");
+export const ChainId = {
+  MAINNET: network.id,
+};
 
+export const routerAddress = "0x6789f48F8ED4506362592Ca58E9c160d624932B6";
