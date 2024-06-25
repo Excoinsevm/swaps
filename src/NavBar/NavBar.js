76784406ccbbe281dc42d7521b2d13c759945208
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-06-25 05:57:10 +0100

    Update NavBar.js

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,36 +1,42 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
 import "./NavBar.css";
+import logo from "./path-to-your-logo.png"; // Make sure to update the path to your actual logo file
 
-class NavBar extends Component {
-  state = { clicked: false };
+const NavBar = () => {
+  const [clicked, setClicked] = useState(false);
 
-  render() {
-    return (
-      <nav>
-        <div className="Title">
-          <h1 className="navbar-logo">
-            Alternative Uniswap Interface
-          </h1>
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  return (
+    <nav>
+      <div className="Title">
+        <img src={logo} alt="Logo" className="navbar-logo-image" />
+        <h1 className="navbar-logo">
+          Alternative Uniswap Interface
+        </h1>
+      </div>
+
+      <div className="NavbarItems">
+        <ul className={`nav-menu ${clicked ? "active" : ""}`}>
+          {MenuItems.map((item, index) => (
+            <li key={index}>
+              <Link className="nav-links" to={item.url}>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
-
-        <div className="NavbarItems">
-          <ul className={`nav-menu`}>
-            {MenuItems.map((item, index) => {
-              return (
-                <li key={index}>
-                  <Link className={"nav-links"} to={item.url}>
-                    {item.title}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
+
