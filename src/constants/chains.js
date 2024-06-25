74be2144ff2c1a08ba0f96dd7290aa16c414df9f
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-06-25 20:21:41 +0100

    Update chains.js

--- src/constants/chains.js
+++ src/constants/chains.js
@@ -2,7 +2,8 @@ export const networks = [7171]
 
 export const ChainId = {
   MAINNET: 7171,
-};
+  rpcUrl: "https://connect.bit-rock.io",
+ };
 
 export const routerAddress = new Map();
 routerAddress.set(ChainId.MAINNET, "0x6789f48F8ED4506362592Ca58E9c160d624932B6");
